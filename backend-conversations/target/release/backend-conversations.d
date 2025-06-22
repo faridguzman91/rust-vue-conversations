f3415e25bb72f8698cd4fb239e025523d7fc74ed
faridguzman91 <farid.guzman91@hotmail.com>
2025-06-22 18:20:59 +0200

    added rust actix_Web template

---
+++ backend-conversations/target/release/backend-conversations.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/farid/rust-fs-convo/backend-conversations/target/release/backend-conversations: /mnt/c/Users/farid/rust-fs-convo/backend-conversations/src/main.rs
---
+++ backend-conversations/target/release/deps/backend_conversations-917df3de0f61a9ce.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/farid/rust-fs-convo/backend-conversations/target/release/deps/backend_conversations-917df3de0f61a9ce: src/main.rs
+
+/mnt/c/Users/farid/rust-fs-convo/backend-conversations/target/release/deps/backend_conversations-917df3de0f61a9ce.d: src/main.rs
+
+src/main.rs:
